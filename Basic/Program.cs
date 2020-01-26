ce6f334cbde9585b62e2754d58c0efe273648d29
Peterson Onyeukwu <10350886+petey284@users.noreply.github.com>
2020-01-25 22:16:04 -0500

    adds initial work

---
+++ Basic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basic
+{
+    public class Processor
+    {
+        public static List<string> GetArgs()
+        {
+            return new List<string>();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myArgs = Processor.GetArgs();
+
+            // Hardcode the playlist url for now
+            var playlistUrl = "PL4cTJUshV2MCpGQuYti2_x0DLYkflTdNX";
+
+            // I need the playlist url as one of the arguments
+            var cmdPrompt = new Standard("C:\\Windows\\System32\\cmd.exe");
+
+            // var environmentOutput= cmdPrompt.RunCommand($"/k echo %PATH%");
+
+            var output = cmdPrompt.Run($"/c youtube-dl.bat -j --flat-playlist {playlistUrl} ^| jq -r \".id\"", true);
+            Console.WriteLine(output.TakeContent());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic/Subprocess.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Basic
+{
+    class Subprocess
+    {
+    }
+
+    public class GenericConsoleApplication
+    {
+        public string Application { get; set; }
+        public Process Proc { get; set; }
+
+        public string LogDbpath { get; set; }
+
+        public string LastOutput;
+
+        private string _allContent;
+        public string AllContent
+        {
+            get { return this._allContent; }
+            set
+            {
+                this.LastOutput = value;
+                this._allContent += value;
+            }
+        }
+        public bool IsSetup { get; set; }
+
+        public GenericConsoleApplication(string application, string logDb=null)
+        {
+            if (!this.VerifyApplication(application))
+            {
+                throw new System.IO.FileNotFoundException($"Application: {application} does not exist.");
+            }
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = application,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            };
+            this.Proc = new Process
+            {
+                StartInfo = startInfo
+            };
+            this.LogDbpath = logDb ?? string.Empty;
+        }
+
+        static void CaptureOutput(object sender, DataReceivedEventArgs e)
+        {
+            ShowOutput(e.Data);
+        }
+
+        static void CaptureError(object sender, DataReceivedEventArgs e)
+        {
+            ShowOutput(e.Data);
+        }
+
+        static void ShowOutput(string data)
+        {
+            if (data != null)
+            {
+                Console.WriteLine(data);
+            }
+        }
+
+        void AddContent(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null) { this._allContent += e.Data + '\n'; }
+        }
+
+        public bool VerifyApplication(string application)
+        {
+            return true;
+        }
+
+        public string TakeContent()
+        {
+            var currentContent = this.AllContent;
+            this.AllContent = "";
+            return currentContent;
+        }
+        
+        void StoreInDb()
+        {
+            // using this.LogDbpath
+            Console.WriteLine(this.AllContent);
+        }
+
+        public GenericConsoleApplication Start(string args)
+        {
+            var argsAsArray = args.Split(' ');
+
+            // return Start(argsAsArray);
+            return this;
+        }
+
+        private void Setup(bool printToConsole, bool wait_for_exit = true)
+        {
+            if (!this.IsSetup)
+            {
+                this.Proc.OutputDataReceived += AddContent;
+                this.Proc.ErrorDataReceived += AddContent;
+
+                if (printToConsole)
+                {
+                    this.Proc.OutputDataReceived += CaptureOutput;
+                    this.Proc.ErrorDataReceived += CaptureError;
+
+                    // I have no idea why this works?
+                    // from: http://www.blackwasp.co.uk/CaptureProcessOutput.aspx
+                    this.Proc.BeginOutputReadLine();
+                    this.Proc.BeginErrorReadLine();
+
+                } else
+                {
+                    this.Proc.StartInfo.RedirectStandardError = true;
+                    this.Proc.StartInfo.RedirectStandardOutput = true;
+                    this.Proc.StartInfo.CreateNoWindow = true;
+                    this.Proc.StartInfo.UseShellExecute = false;
+                    this.Proc.EnableRaisingEvents = true;
+                }
+
+                this.Proc.Start();
+
+                //// I have no idea why this works?
+                //// from: http://www.blackwasp.co.uk/CaptureProcessOutput.aspx
+                this.Proc.BeginOutputReadLine();
+                this.Proc.BeginErrorReadLine();
+
+                if (wait_for_exit) { this.Proc.WaitForExit(); }
+
+                if (this.LogDbpath != null) { this.StoreInDb(); }
+
+                this.IsSetup = true;
+            }
+
+
+        }
+
+        public GenericConsoleApplication Start(string[] args, bool wait_for_exit = true)
+        {
+            var unsized = args.ToList();
+            var arguments = string.Empty;
+            foreach (var element in unsized) { arguments += element + " "; }
+
+            this.Proc.StartInfo.Arguments = arguments;
+
+            this.Proc.OutputDataReceived += CaptureOutput;
+            this.Proc.OutputDataReceived += AddContent;
+            this.Proc.ErrorDataReceived += CaptureError;
+            
+            this.Proc.Start();
+
+
+            // I have no idea why this works?
+            // from: http://www.blackwasp.co.uk/CaptureProcessOutput.aspx
+            this.Proc.BeginOutputReadLine();
+            this.Proc.BeginErrorReadLine();
+
+            if (wait_for_exit) { this.Proc.WaitForExit(); }
+
+            if (this.LogDbpath != null) { this.StoreInDb(); } 
+
+            return this;
+        }
+
+        public GenericConsoleApplication StartWithArgs(string args, bool wait_for_exit = true)
+        {
+            var arguments = args;
+
+            this.Proc.StartInfo.Arguments = arguments;
+            this.Setup(false, wait_for_exit);
+
+            return this;
+        }
+
+        public GenericConsoleApplication RunAndPrint(string args, bool wait_for_exit = true)
+        {
+            var arguments = args;
+
+            this.Proc.StartInfo.Arguments = arguments;
+            this.Setup(true, wait_for_exit);
+
+            return this;
+        }
+
+        public GenericConsoleApplication Run(string args, bool wait_for_exit = true)
+        {
+            var arguments = args;
+
+            this.Proc.StartInfo.Arguments = arguments;
+            this.Setup(false, wait_for_exit);
+
+            return this;
+        }
+    }
+
+    public class Standard : GenericConsoleApplication
+    {
+        public Standard(string application)
+            : base(application)
+        {
+
+        }
+
+        public string RunCommand(string arguments, bool waitUntilFinished=false)
+        {
+            return this.StartWithArgs(arguments, waitUntilFinished).TakeContent();
+        }
+    }
+}
